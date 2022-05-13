861e19f054153832bb84d6b3dc4bc66bed1efacd
Abhishek Parmar <abhishekparmar220197@gmail.com>
2022-05-13 10:21:43 +0530

    First section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
 function App() {
+
   return (
-    <button>Click Me</button>
-          );
+    <>
+    <section id="hero-section">
+      <div>
+        <h1>Organic Plants  <br/>And Meet Products</h1>
+        
+          <button id="button">Discover now</button>
+      </div>
+    </section>
+    <section id="food">
+      <div>
+          <div>
+            <h1>Food Categories</h1>
+            <p>A paragraph is a series of sentences that are organized and coherent, and are all related to a single topic. Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs.</p>
+          </div>
+      </div>
+    </section>
+</>
+  );
 }
 
 export default App;
