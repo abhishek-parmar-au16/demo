090ae2ed291f65faab90ff873806c8216afa2870
Abhishek Parmar <abhishekparmar220197@gmail.com>
2022-05-17 12:00:07 +0530

    Done

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,6 +4,13 @@ import '../App.css'
 const Hero = () => {
     return (
         <section id="hero-section">
+            <div style = {{display:"flex",color:"white",paddingLeft: "26rem"}}>
+                <h4 style={{margin:"1rem",fontSize: "larger"}}>Home</h4>
+                <h4 style={{margin:"1rem",fontSize: "larger"}}>About us</h4>
+                <h4 style={{margin:"1rem",fontSize: "larger"}}>Products</h4>
+                <h4 style={{margin:"1rem",fontSize: "larger"}}>Cooperation</h4>
+                <h4 style={{margin:"1rem",fontSize: "larger"}}>Contact Us</h4>
+            </div>
             <div>
                 <h1>Organic Plants  <br/>And Meet Products</h1>
                 
---
+++ src/Components/endPoints.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "https://waycool-project1-backend.herokuapp.com"
+
+const API_BASE_URL = `${BASE_URL}/api`
+export const homepageApi = `${API_BASE_URL}/homepage`
+export const productsApi = `${API_BASE_URL}/products`
