23340233dc04ca9644c348ae4d6577933713104e
Abhishek Parmar <abhishekparmar220197@gmail.com>
2022-05-17 11:58:44 +0530

    ABOUT US

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,71 @@
 import React from 'react';
 import '../App.css'
 import guy from '../images/ecofood-guy-in-field.jpg'
-
+import axios from 'axios';
+import {useState ,useEffect} from "react"
 const About = () => {
-    return (
+
+    const [data, setData] = useState([]);
+    const [loaded,setLoaded] =useState(false)
+    const [categories ,setCategories] = useState([])
+
+    const BaseUrl = "https://waycool-project1-backend.herokuapp.com"
+    console.log(BaseUrl + data.heroSection?.backgroundImage);
+    
+
+    // useEffect(() => {
+    //     let unmounted = false
+    //     const getdata = async ()={
+    //         try {
+    //             const fetchResponse = await axios.get("https://waycool-project1-backend.herokuapp.com/api/homepage")
+    //             const fetchData = fetchResponse.data
+    //             if (!unmounted){
+    //                 setData(prev =>{
+    //                     (
+    //                         ...prev,
+    //                         heroSection:{
+    //                             title:!fetchData.heroSection?.title ?"": fetchData.heroSection.title ,
+    //                             bgimg : !!fetchData.heroSection?.backgroundImage ? `${BaseUrl}/${fetchData.heroSection.backgroundImage}`:"",
+    //                             buttonText :!! fetchData.heroSection?.buttonText ? fetchData.heroSection.buttonText :''
+    //                         },
+    //                     )})
+    //                     setCategories(!!fetchData.foodCategories ? fetchData.foodCategories.map(item => ({...item,id:item._id,image:`${BaseUrl}/${item.image}` })) :[])
+    //             }
+    //         }catch (err){
+    //             setData({})
+    //             setCategories([])
+    //         }
+    //         setLoaded(true)
+    //     }
+    //     getdata()
+    //     return()=>{
+    //         unmounted=true
+    //     }
+        
+    // }, []);
+    
+    useEffect((token) => {
+
+        const res = axios.get("https://waycool-project1-backend.herokuapp.com/api/homepage", {
+            headers: {
+                'Content-Type': 'application/json',
+                // 'authorization': userDetails.token
+            }
+        }).then((response) => {
+            if (response.data) {
+                console.log("krunal", response.data)
+    setData(response.data)
+                //  setAdoptionCount(fetchonecountdata)
+            }
+        })
+            .catch((err) => console.log(err));
+    }, []);
+
+    // if (!loaded){
+    //     return(<div>Loading.....</div>)
+    // }
+
+    return (<>
         <section id="about">
             <div>
                 <div>
@@ -15,12 +77,23 @@ const About = () => {
                         <p>Farmers are the lifeblood of the Indian agricultural system.<br /> They sow and reap crops. When the right season arrives,<br /> they also harvest crops. In spite of being such an important part of the economy,<br /> they are marginalized and impoverished.</p>
                     </div>
                     <div clasName="aboutimage">
-                            <img  src={guy} alt="Farmer "></img>
+                            {/* <img  src={guy} alt="Farmer "></img> */}
                     </div>
                 </div>
             </div>
             
         </section>
+        <section id="api">
+            
+            <div>
+               <h1> {data.heroSection?.title}</h1>
+               <img  className="kuchnhi" src={BaseUrl +'/'+ data.heroSection?.backgroundImage} alt="pat" />
+            </div>
+
+            
+            
+        </section>
+        </>
     );
 };
 
