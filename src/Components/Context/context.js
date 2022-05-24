69ebee84692ad0116d0c6cbbd5f21434456134b7
Abhishek Parmar <abhishekparmar220197@gmail.com>
2022-05-24 12:59:30 +0530

    SetUp

--- src/Components/Context/context.js
+++ src/Components/Context/context.js
@@ -1,12 +1,77 @@
-import React,{createContext} from 'react';
+import React,{createContext , useReducer} from 'react';
 
-const context = () => {
 
-    return (
-        <div>
+export const CounterContext = createContext({count:5})
+const initialstate = 0
+
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+const reducer = (state,action) =>{
+    switch (action.type) {
+        case INCREMENT:
+            return state + 1
             
-        </div>
-    );
-};
+        case DECREMENT:
+            return state - 1
+        default:
+            return state
+    }
+}
 
-export default context;
+
+function CounterProvider({children}) {
+    const [state,dispatch] = useReducer(reducer,initialstate)
+    return  <CounterContext.Provider value={{count:state , updateCount:dispatch }} >
+                {children}
+            </CounterContext.Provider >
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Count from './Count.js'
+
+// const context = () => {
+//     export const UserContext = createContext();
+//     return (
+//         <UserContext.Provider >
+//             <Count />
+//         </UserContext.Provider>
+//     );
+// };
+
+// export default context;
